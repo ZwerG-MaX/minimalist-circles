98dc13dbf0282fab298c829dd84fe6f1b4ca5d58
James Dyke <james.dyke91@gmail.com>
2017-05-01 16:25:35 +0200

    First CPU (Pre loop)

--- circle.lua
+++ circle.lua
@@ -5,13 +5,14 @@
 
 require 'cairo'
 
+local FONT, ICON_FONT = "Mono", "StyleBats"
+
 function init_cairo()
     if conky_window == nil then
         return false
     end
 
-    cs = cairo_xlib_surface_create(
-        conky_window.display,
+    cs = cairo_xlib_surface_create(conky_window.display,
         conky_window.drawable,
         conky_window.visual,
         conky_window.width,
@@ -19,10 +20,8 @@ function init_cairo()
 
     cr = cairo_create(cs)
 
-    font = "droid"
-
-    cairo_select_font_face(cr, font, CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL)
-    cairo_set_source_rgba(cr, 0.933,0.905,0.894,1)
+    cairo_select_font_face(cr, FONT, CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL)
+    cairo_set_source_rgba(cr, 0.933, 0.905, 0.894, 1)
     return true
 end
 
@@ -32,18 +31,56 @@ function conky_main()
     end
 
     draw_date_and_time()
+    draw_disk_usage()
+    draw_cpu_usage()
 end
 
 function draw_date_and_time()
-    -- TIME
+    -- Time
     cairo_set_font_size(cr, 130)
     cairo_move_to(cr, 750, 270)
     cairo_show_text(cr, conky_parse("${time %H:%M}"))
     cairo_stroke(cr)
 
-    -- DATE
+    -- Date
     cairo_set_font_size(cr, 55)
     cairo_move_to(cr, 778, 350)
     cairo_show_text(cr, conky_parse("${time %Y/%m/%d}"))
+    cairo_stroke(cr)
+end
+
+function draw_disk_usage()
+    cairo_move_to(cr, 40, 510)
+    cairo_set_font_size(cr, 30)
+    cairo_show_text(cr, conky_parse("/ at ${fs_used_perc /}%"))
+    local diskRoot = (tonumber(conky_parse("${exec df -h / | grep -w / | awk '{print $5}' | sed 's/%//g'}")) / 100 * 360)
+    cairo_stroke(cr)
+
+    -- cairo_arc requires radions
+    local angle2 = diskRoot * (math.pi / 180)
+    local center_x = 110
+    local center_y = 500
+    local radius = 85
+    local start_angle = 0
+    cairo_set_line_width(cr, 5.0)
+    cairo_arc(cr, center_x, center_y, radius, start_angle, angle2)
+    cairo_stroke(cr)
+end
+
+function draw_cpu_usage()
+    cairo_move_to(cr, 413, 905)
+    local cpu0 = tonumber(conky_parse("${cpu cpu0}"))
+    cairo_show_text(cr, "Cpu0")
+    cairo_stroke(cr)
+    print(cpu0)
+
+    --    local angle2 = (cpu0 - 180 ) * (math.pi / 180)
+    local angle2 = (cpu0/100) * 360
+    local center_x = 450
+    local center_y = 900
+    local radius = 85
+    local start_angle = 0
+    cairo_set_line_width(cr, 5.0)
+    cairo_arc(cr, center_x, center_y, radius, (start_angle - 180) * (math.pi/180), (angle2 - 180) * (math.pi/180))
     cairo_stroke(cr)
 end
